c86d6633a6907f91a76dcba9020657d07bb26663
Sandeep Yadav <33416624+javalearnerss@users.noreply.github.com>
2026-04-18 23:26:56 +0530

    auth server app is removed

--- EKS/trade-booking-microservices/chapter-01/01-apps/java-apps/trading-auth-server/src/main/java/com/learn/microservices/trading/auth/server/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learn.microservices.trading.auth.server.repository;
-
-
-import com.learn.microservices.trading.auth.server.identity.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,String> {}
